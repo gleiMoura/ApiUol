7ecf9c001fc596febe40a1ffb763075866ee6291
gleiMoura <gle.bar.mou@gmail.com>
2023-02-23 19:54:56 -0300

    create participant router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import Router from "express";
+import participantRouter from "./participantRouter";
 
 const router = Router();
+
+router.use(participantRouter);
 
 export default router;
---
+++ src/routes/participantRouter.ts
@@ -0,0 +1,7 @@
+import Router from "express";
+
+const participantRouter = Router();
+
+participantRouter.post("/participants")
+
+export default participantRouter;
