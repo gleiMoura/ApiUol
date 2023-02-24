f1a53b128a807ac30643dab4dcdd826716465609
gleiMoura <gle.bar.mou@gmail.com>
2023-02-24 07:45:54 -0300

    :sparkled: feat: create a sechema folder with participant schema and put it inside the schema validator at participantRouter

--- src/routes/participantRouter.ts
+++ src/routes/participantRouter.ts
@@ -1,7 +1,9 @@
 import Router from "express";
+import schemaValidator from "../middlewares/schemaValidator";
+import participantSchema from "../schemas/participantSchema";
 
 const participantRouter = Router();
 
-participantRouter.post("/participants")
+participantRouter.post("/participants",schemaValidator(participantSchema));
 
 export default participantRouter;
