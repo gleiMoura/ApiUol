068f07e2d44805b77ef2bb0b2c5b4cfbe0ae1de0
gleiMoura <gle.bar.mou@gmail.com>
2023-04-01 20:57:01 -0300

    :test_tube: test: create a test to get all participants

--- tests/unit.spec.ts
+++ tests/unit.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
 import participantRepository from "../src/repository/participantRepository";
 import participantService from "../src/services/participantService";
+import { participantType } from ".././src/interfaces/index"
 
 describe("Unit Services tests", () => {
     it("should create a participant", async () => {
@@ -20,4 +21,20 @@ describe("Unit Services tests", () => {
         expect(participantRepository.createParticipant).toBeCalled();
         expect(participantRepository.createEnterMessage).toBeCalled();
     });
+
+    it("should get all participants", async () => {
+        const participants:participantType[] = [
+            {name: faker.name.firstName()},
+            {name: faker.name.firstName()},
+            {name: faker.name.firstName()},
+            {name: faker.name.firstName()},
+        ];
+
+        jest.spyOn(participantRepository, "findAllParticipants")
+        .mockImplementation(async () => participants);
+
+        const names = await participantService.getAllParticipants();
+
+        expect(names).toBe(participants);
+    })
 })
