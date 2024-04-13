378c3d883c528042209863277752446db06d9f20
gleiMoura <gle.bar.mou@gmail.com>
2024-04-13 00:09:56 -0300

    ♻️ refactor: Put the function out of app

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,14 @@ import router from "./routes/index.js";
 import errorHandler from "./middlewares/errorValidator.js";
 import participantService from "services/participantService.js";
 
+const repeatedFunction = participantService.removeParticipant;
+
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(router);
 app.use(errorHandler);
-app.use(participantService.removeParticipant)
+
+repeatedFunction();
 
 export default app;
