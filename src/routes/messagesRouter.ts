ecbbf617e1094c5b95230173a5d3daba8fee4e93
gleiMoura <gle.bar.mou@gmail.com>
2024-04-13 22:22:05 -0300

    ♻️ refactor: change sendMessages to getAllMessages to improve the sintax

--- src/routes/messagesRouter.ts
+++ src/routes/messagesRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import messageSchema from "../schemas/messageSchema.js";
-import { createMessage, sendMessages } from "../controllers/messageController.js";
+import { createMessage, getAllMessages } from "../controllers/messageController.js";
 
 const messagesRouter = Router();
 
 messagesRouter.post("/messages", schemaValidator(messageSchema), createMessage);
-messagesRouter.get("/messages", sendMessages);
+messagesRouter.get("/messages", getAllMessages);
 
 export default messagesRouter;
