b2b78288fb0b15ecb1e48911da8e4cc9ad1f8789
gleiMoura <gle.bar.mou@gmail.com>
2024-04-22 16:01:14 -0300

    ✨ feat: create router to update the message

--- src/routes/messagesRouter.ts
+++ src/routes/messagesRouter.ts
@@ -1,12 +1,18 @@
-import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import messageSchema from "../schemas/messageSchema.js";
-import { createMessage, getAllMessages, removeMessage } from "../controllers/messageController.js";
+import { Router } from "express";
+import {
+    createMessage,
+    getAllMessages,
+    removeMessage,
+    changeMessage
+} from "../controllers/messageController.js";
 
 const messagesRouter = Router();
 
 messagesRouter.post("/messages", schemaValidator(messageSchema), createMessage);
 messagesRouter.get("/messages", getAllMessages);
-messagesRouter.delete("/messages/:id", removeMessage)
+messagesRouter.delete("/messages/:id", removeMessage);
+messagesRouter.put("/messages/:id", schemaValidator(messageSchema), changeMessage);
 
 export default messagesRouter;
