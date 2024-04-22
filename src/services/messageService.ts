adb301a292f317cd679768eb194fb33589db101e
gleiMoura <gle.bar.mou@gmail.com>
2024-04-22 16:03:00 -0300

    ✨ feat: create a service to update the message

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -5,6 +5,7 @@ import participantRepository from "../repository/participantRepository";
 import messageRepository from "../repository/messagesRepository";
 import { limitType, userType } from "../interfaces/index";
 import messagesRepository from "../repository/messagesRepository";
+import { ObjectId } from "mongodb";
 
 async function gatherDatas(data: messageType, user: userType) {
     const participant = await participantRepository.findParticipant(user);
@@ -81,10 +82,49 @@ async function deleteMessage(id: any, user: userType) {
     }
 
     await messageRepository.deleteMessageById(id);
+};
+
+async function updateMessage(user, id, newMessage) {
+    const existMessage: any = await messageRepository.findMessageById(id);
+
+    const existParticipant: any = await participantRepository.findParticipant(user);
+
+    if (!existParticipant) {
+        throw {
+            response: {
+                status: 422,
+                message: "Participante não encontrado!"
+
+            }
+        }
+    }
+
+    if (!existMessage) {
+        throw {
+            response: {
+                status: 404,
+                message: "Mensagem nao encontrada!"
+
+            }
+        }
+    }
+
+    if (existMessage.from !== user) {
+        throw {
+            response: {
+                status: 401,
+                message: "Não é dono!"
+
+            }
+        }
+    }
+
+    await messageRepository.modernizeMessage({ ...newMessage, from: user, id: new ObjectId(id) }, id);
 }
 
 export default {
     gatherDatas,
     getMessages,
-    deleteMessage
+    deleteMessage,
+    updateMessage
 };
