45d819f40812899b907d1ccf523698c7a18f90fd
gleiMoura <gle.bar.mou@gmail.com>
2024-04-23 11:08:18 -0300

    🐛 fix: name must have just number and letters. Not at all.

--- src/schemas/participantSchema.ts
+++ src/schemas/participantSchema.ts
@@ -1,7 +1,14 @@
 import joi from "joi";
 
 const participantSchema = joi.object({
-    name: joi.string().required()
+    name: joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]+$'))
+        .required()
+        .messages({
+            'string.pattern.base': `"nome" deve conter apenas letras e números sem espaços ou caracteres especiais`,
+            'string.empty': `"nome" não pode estar vazio`,
+            'any.required': `"nome" é obrigatório`
+        })
 });
 
 export default participantSchema;
