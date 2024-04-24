068b9800b078645e3948fd85240dd00b41cdbad1
gleiMoura <gle.bar.mou@gmail.com>
2024-04-24 15:54:38 -0300

    🐛 fix: put router to be used

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import Router from "express";
 import participantRouter from "./participantRouter.js";
 import messagesRouter from "./messagesRouter.js"
+import statusRouter from "./statusRouter.js";
 
 const router = Router();
 
 router.use(participantRouter);
 router.use(messagesRouter);
+router.use(statusRouter)
 
 export default router;
