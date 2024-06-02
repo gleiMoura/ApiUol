a4337e09e21ee44152892a2dff163cad6ae68325
gleiMoura <gle.bar.mou@gmail.com>
2024-06-01 22:55:26 -0300

    🔧 adding mongo collection directly in config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const Client = process.env.MONGO_URL ? process.env.MONGO_URL : process.env.MONGO_URL_DEV;
+
 async function init() {
-  const mongoClient = new MongoClient(process.env.MONGO_URL || "");
+  const mongoClient = new MongoClient(Client);
   await mongoClient.connect();
   console.log("Database is connected!");
-  return mongoClient.db(process.env.BANCO);
+  const database = mongoClient.db(process.env.BANCO);
+
+  await database.createCollection('messages');
+  await database.createCollection('participants');
+
+  return database;
 }
 
 const db = init();
