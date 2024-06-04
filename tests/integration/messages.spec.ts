2752ed790ad9cf645382894e535cf697d12f505d
gleiMoura <gle.bar.mou@gmail.com>
2024-06-04 17:29:30 -0300

    ✅ add spec tests scope

---
+++ tests/integration/messages.spec.ts
@@ -0,0 +1,35 @@
+describe("POST /messages", () => {
+    it("Given a valid message and user name it must return 201", () => {
+
+    });
+
+    it("Given an invalid user name it must return error 409", () => {
+
+    });
+
+    it("Given an invalid message ir must return error 410", () => {
+
+    });
+});
+
+describe("GET /messages", () => {
+    it("Given a valid user name it must return 201", () => {
+
+    });
+
+    it("Given a valid user name and a valid massage limit it must return 201", () => {
+
+    });
+
+    it("Given an invalid user name it must return error 410", () => {
+
+    });
+
+    it("Given an invalid limit it must return 201", () => {
+
+    });
+
+    it("Given a valid user name it must return 201", () => {
+
+    });
+})
