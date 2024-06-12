ea4c1d7b0813508e529a1953c9eb3b62c09ec93f
gleiMoura <gle.bar.mou@gmail.com>
2024-06-11 23:55:57 -0300

    ✅ add mote two integrations tests

--- tests/integration.messages.spec.ts
+++ tests/integration.messages.spec.ts
@@ -3,6 +3,14 @@ import supertest from "supertest";
 import factories from "./factories/index";
 import db from "../src/config/index";
 
+afterEach(async () => {
+    const database = await db;
+    const collections = await database.collections();
+    for(let collection of collections) {
+        await collection.deleteMany({});
+    }
+})
+
 describe("POST /messages", () => {
     it("Given a valid message and user name it must return 201", async () => {
         const message = factories.fakeMessage;
@@ -20,17 +28,52 @@ describe("POST /messages", () => {
 
         const database = await db;
         const createdMessage = await database.collection("messages").findOne({ from: user });
-        
+
         expect(status).toEqual(201);
         expect(createdMessage).not.toBeNull();
     });
 
-    it("Given an invalid user name it must return error 409", () => {
+    it("Given an invalid user name it must return error 409", async () => {
+        const message = factories.fakeMessage;
+        const user = factories.fakeParticipant + "*" //error;
 
+        await supertest(app)
+            .post("/participants").send({ name: user })
+
+        const result = await supertest(app)
+            .post("/messages")
+            .set('User', user)
+            .send(message);
+
+        const status = result.status;
+
+        const database = await db;
+        const createdMessage = await database.collection("messages")
+            .findOne({ from: user });
+
+        expect(status).toEqual(409);
+        expect(createdMessage).toBeNull();
     });
 
-    it("Given an invalid message ir must return error 410", () => {
+    it("Given an invalid message it must return error 410", async () => {
+        const user = factories.fakeParticipant;
+        const message = ""
 
+        await supertest(app)
+            .post("/participants").send({ name: user })
+
+        const result = await supertest(app)
+            .post("/messages")
+            .set('User', user)
+            .send(message);
+
+        const status = result.status;
+
+        const database = await db;
+        const createdMessage = await database.collection("messages").findOne({ from: user, type: "message" || "private_message" });
+
+        expect(status).toEqual(410);
+        expect(createdMessage).toBeNull();
     });
 });
 
