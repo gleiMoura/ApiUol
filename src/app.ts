f7609823d32c51b09e7772bcb9a661b91166d94b
gleiMoura <gle.bar.mou@gmail.com>
2024-07-23 19:33:14 -0300

    refact: change cors

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,21 @@ const repeatedFunction = participantService.removeParticipant;
 
 const app = express();
 app.use(cors({
-    origin: 'https://frontuol.onrender.com',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    origin: 'https://frontuol.onrender.com'
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontuol.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
 app.use(json());
 app.use(router);
 app.use(errorHandler);
