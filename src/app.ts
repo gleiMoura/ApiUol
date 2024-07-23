7ba87ad9a5d8d60111a487ae040155ae458cb411
gleiMoura <gle.bar.mou@gmail.com>
2024-07-23 19:28:06 -0300

    🌱  refact: changing cors config

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,9 @@ const repeatedFunction = participantService.removeParticipant;
 
 const app = express();
 app.use(cors({
-    origin: 'https://frontuol.onrender.com'
+    origin: 'https://frontuol.onrender.com',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 }));
 app.use(json());
 app.use(router);
