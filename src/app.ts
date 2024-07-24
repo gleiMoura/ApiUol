99270ef56795dd1e6d77c981d11d904352585e48
gleiMoura <gle.bar.mou@gmail.com>
2024-07-24 11:49:35 -0300

    ♻️ changing cors

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,18 @@ import participantService from "./services/participantService";
 const repeatedFunction = participantService.removeParticipant;
 
 const app = express();
+
 app.use(cors({
     origin: 'https://frontuol.onrender.com'
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://frontuol.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
     if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Origin', 'https://frontuol.onrender.com');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         return res.status(200).json({});
     }
-
     next();
 });
 
